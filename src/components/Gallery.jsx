7d61d270e593f357842628426d079a15dbf55ca1
shounaksarker <shounaksarker@gmail.com>
2023-11-04 12:53:15 +0600

    project done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { ImageBox } from "@/components/ImageBox";
 import { ImageUploader } from "@/components/ImageUploader";
@@ -15,23 +15,27 @@ import img10 from "@/images/image-10.jpeg";
 import img11 from "@/images/image-11.jpeg";
 
 export const Gallery = () => {
-  const imgArr = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img11,
+  // <----- array of all images ----->
+  const initialImgArr = [
+    { id: 1, img: img1 },
+    { id: 2, img: img2 },
+    { id: 3, img: img3 },
+    { id: 4, img: img4 },
+    { id: 5, img: img5 },
+    { id: 6, img: img6 },
+    { id: 7, img: img7 },
+    { id: 8, img: img8 },
+    { id: 9, img: img9 },
+    { id: 10, img: img10 },
+    { id: 11, img: img11 },
   ];
 
+  // <----- States -----> 
+  const [imgArr, setImgArr] = useState(initialImgArr);
   const [selectedImages, setSelectedImages] = useState([]);
   const [selectAll, setSelectAll] = useState(false);
 
+  // <----- functions for select items ----->
   const handleCheckboxChange = (event, imgName) => {
     if (event.target.checked) {
       setSelectedImages([...selectedImages, imgName]);
@@ -39,14 +43,40 @@ export const Gallery = () => {
       setSelectedImages(selectedImages.filter((img) => img !== imgName));
     }
   };
-
+// <----- select all images ----->
   const handleSelectAllChange = (event) => {
     setSelectAll(event.target.checked);
     setSelectedImages(event.target.checked ? imgArr : []);
   };
 
+
+  // <----- Function for delete selected images ----->
+  const handleDelete = () => {
+    // Filter out the selected images from imgArr
+    const updatedImgArr = imgArr.filter((img) => !selectedImages.includes(img));
+    setImgArr(updatedImgArr);
+    setSelectedImages([]);
+    setSelectAll(false);
+  };
+
+  
+  // <----- functions for drag and drop ----->
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/plain", index);
+  };
+
+  const handleDrop = (e, newIndex) => {
+    e.preventDefault();
+    const draggedIndex = e.dataTransfer.getData("text/plain");
+    const updatedItems = [...imgArr];
+    updatedItems.splice(newIndex, 0, updatedItems.splice(draggedIndex, 1)[0]);
+    setImgArr(updatedItems);
+  };
+
   return (
-    <section className="bg-white max-w-[90%] mx-auto rounded-lg py-4">
+    <section className="bg-white max-w-[90%] lg:max-w-[70%] 2xl:max-w-[90%] mx-auto rounded-lg py-4">
+      {/* <----- top bar -----> */}
       <div className="border-b border-slate-400 px-4 pb-2">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-3">
@@ -62,20 +92,39 @@ export const Gallery = () => {
                 : "Gallery"}
             </label>
           </div>
-          <button className="text-red-500 font-semibold">Delete File</button>
+          <button
+            className="text-red-500 font-semibold"
+            onClick={handleDelete}
+            disabled={selectedImages.length === 0}
+          >
+            Delete File
+          </button>
         </div>
       </div>
+
+      {/* <----- all images -----> */}
       <div className="p-4 w-full mx-auto">
-        <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-3 w-full">
-          {imgArr?.map((imgg, index) => (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 w-full">
+          {imgArr?.map((pic, index) => (
             <div
               className={`aspect-square ${
-                index === 0 && "md:row-span-2 md:col-span-2"
+                index === 0 && "col-span-2 row-span-1 md:row-span-2"
               }`}
+              key={index}
+              draggable="true"
+              onDragStart={(e) => handleDragStart(e, index)}
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={(e) => handleDrop(e, index)}
             >
-              <ImageBox img={imgg} onCheckboxChange={handleCheckboxChange} isSelected={selectedImages.includes(imgg)}/>
+              <ImageBox
+                img={pic}
+                onCheckboxChange={handleCheckboxChange}
+                isSelected={selectedImages.includes(pic)}
+              />
             </div>
           ))}
+
+          {/* <----- image uploder -----> */}
           <div className="aspect-sqare h-full">
             <ImageUploader />
           </div>
--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -6,11 +6,19 @@ export const ImageBox = ({ img, onCheckboxChange, isSelected }) => {
   };
   return (
     <div className="relative w-full h-full rounded-lg border border-slate-400 transition-all group/item">
-      <Image src={img} alt="img" fill className="rounded-lg z-10" />
-      <div className={`group/edit invisible group-hover/item:visible w-full h-full z-20 absolute rounded-lg opacity-50 ${isSelected ? "bg-white !visible" : "bg-black"}`} />
+      <Image src={img.img} alt="img" fill className="rounded-lg z-10" />
+      {/* <----- overlay over the image on hover ----->  */}
+      <div
+        className={`group/edit invisible transition-all group-hover/item:visible w-full h-full z-20 absolute rounded-lg opacity-50 ${
+          isSelected ? "bg-white !visible" : "bg-black"
+        }`}
+      />
+      {/* <----- checkbox for selecting an image -----> */}
       <input
         type="checkbox"
-        className={`group/edit invisible group-hover/item:visible absolute z-30 top-5 left-5 p-2 h-5 w-5  ${isSelected ? "!visible" : ""}`}
+        className={`group/edit invisible group-hover/item:visible absolute z-30 top-5 left-5 p-2 h-5 w-5  ${
+          isSelected ? "!visible" : ""
+        }`}
         onChange={handleCheckbox}
         checked={isSelected}
       />
